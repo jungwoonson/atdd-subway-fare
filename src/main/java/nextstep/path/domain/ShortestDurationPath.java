114fa379ea8c354425d172666adb626b6354a793
jungwoonson <jungwoon727@gmail.com>
2024-09-09 20:35:35 +0900

    refactor: 최소 경로 탐색 중복 코드 제거를 위한 수정

--- src/main/java/nextstep/path/domain/ShortestDurationPath.java
+++ src/main/java/nextstep/path/domain/ShortestDurationPath.java
@@ -1,35 +1,24 @@
 package nextstep.path.domain;
 
 import nextstep.line.domain.Section;
-import nextstep.path.application.exception.NotAddedEndToPathsException;
-import nextstep.path.application.exception.NotAddedStartToPathsException;
-import nextstep.path.application.exception.NotConnectedPathsException;
 import nextstep.station.domain.Station;
-import org.jgrapht.GraphPath;
-import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
-import org.jgrapht.alg.shortestpath.KShortestPaths;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.WeightedMultigraph;
 
 import java.util.List;
 
-public class ShortestDurationPath implements ShortestPath {
+public class ShortestDurationPath extends ShortestPath {
 
-    private List<Section> sections;
-    private WeightedMultigraph<Station, DefaultWeightedEdge> graph;
-    private int maxDuration;
-
-    private ShortestDurationPath(List<Section> sections, WeightedMultigraph<Station, DefaultWeightedEdge> graph, int maxDuration) {
-        this.sections = sections;
-        this.graph = graph;
-        this.maxDuration = maxDuration;
+    public ShortestDurationPath(List<Section> sections) {
+        super(sections);
     }
 
     public static ShortestDurationPath from(List<Section> sections) {
-        return new ShortestDurationPath(sections, createGraph(sections), maxDuration(sections));
+        return new ShortestDurationPath(sections);
     }
 
-    private static WeightedMultigraph<Station, DefaultWeightedEdge> createGraph(List<Section> sections) {
+    @Override
+    protected WeightedMultigraph<Station, DefaultWeightedEdge> createGraph(List<Section> sections) {
         WeightedMultigraph<Station, DefaultWeightedEdge> graph = new WeightedMultigraph<>(DefaultWeightedEdge.class);
         sections.forEach(section -> {
             graph.addVertex(section.getUpStation());
@@ -40,79 +29,19 @@ public class ShortestDurationPath implements ShortestPath {
         return graph;
     }
 
-    private static int maxDuration(List<Section> sections) {
-        return sections.stream()
-                .mapToInt(Section::getDuration)
-                .max()
-                .orElse(0);
-    }
-
-    public List<Station> getStations(Station start, Station end) {
-        validateContains(start, end);
-        GraphPath<Station, DefaultWeightedEdge> paths = findShortestPath(start, end);
-        validateConnected(paths);
-        return paths.getVertexList();
-    }
-
-    private void validateConnected(GraphPath<Station, DefaultWeightedEdge> paths) {
-        if (paths == null) {
-            throw new NotConnectedPathsException();
-        }
-    }
-
-    public void validateConnected(Station start, Station end) {
-        if (findShortestPath(start, end) == null) {
-            throw new NotConnectedPathsException();
-        }
-    }
-
-    public void validateContains(Station start, Station end) {
-        if (!graph.containsVertex(start)) {
-            throw new NotAddedStartToPathsException(start.getName());
-        }
-        if (!graph.containsVertex(end)) {
-            throw new NotAddedEndToPathsException(end.getName());
-        }
-    }
-
-    private GraphPath<Station, DefaultWeightedEdge> findShortestPath(Station start, Station end) {
-        DijkstraShortestPath<Station, DefaultWeightedEdge> dijkstraShortestPath = new DijkstraShortestPath<>(graph);
-        return dijkstraShortestPath.getPath(start, end);
-    }
-
     @Override
     public int getDuration(Station start, Station end) {
         validateContains(start, end);
-        List<GraphPath<Station, DefaultWeightedEdge>> paths = new KShortestPaths<>(graph, maxDuration).getPaths(start, end);
-        validateConnected(paths);
-        GraphPath<Station, DefaultWeightedEdge> shortestPath = paths.get(0);
-        return (int) shortestPath.getWeight();
+        return (int) findShortestPath(start, end).getWeight();
     }
 
     @Override
     public int getDistance(Station start, Station end) {
         validateContains(start, end);
-        GraphPath<Station, DefaultWeightedEdge> paths = findShortestPath(start, end);
-        validateConnected(paths);
-
-        return paths.getEdgeList().stream()
+        return findShortestPath(start, end)
+                .getEdgeList()
+                .stream()
                 .mapToInt(edge -> getSectionByEdge(edge).getDistance())
                 .sum();
-    }
-
-    private Section getSectionByEdge(DefaultWeightedEdge edge) {
-        Station upStation = graph.getEdgeSource(edge);
-        Station downStation = graph.getEdgeTarget(edge);
-
-        return sections.stream()
-                .filter(section -> upStation.equals(section.getUpStation()) && downStation.equals(section.getDownStation()))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("엣지에 해당하는 구간을 찾을 수 없습니다."));
-    }
-
-    private void validateConnected(List<GraphPath<Station, DefaultWeightedEdge>> paths) {
-        if (paths.isEmpty()) {
-            throw new NotConnectedPathsException();
-        }
     }
 }
--- src/main/java/nextstep/path/domain/ShortestPath.java
+++ src/main/java/nextstep/path/domain/ShortestPath.java
@@ -1,18 +1,73 @@
 package nextstep.path.domain;
 
+import nextstep.line.domain.Section;
+import nextstep.path.application.exception.NotAddedEndToPathsException;
+import nextstep.path.application.exception.NotAddedStartToPathsException;
+import nextstep.path.application.exception.NotConnectedPathsException;
 import nextstep.station.domain.Station;
+import org.jgrapht.GraphPath;
+import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
 
 import java.util.List;
 
-public interface ShortestPath {
+public abstract class ShortestPath {
 
-    List<Station> getStations(Station start, Station end);
+    protected List<Section> sections;
+    protected WeightedMultigraph<Station, DefaultWeightedEdge> graph;
 
-    void validateConnected(Station start, Station end);
+    public ShortestPath(List<Section> sections) {
+        this.sections = sections;
+        this.graph = createGraph(sections);
+    }
 
-    void validateContains(Station start, Station end);
+    public List<Station> getStations(Station start, Station end) {
+        validateContains(start, end);
+        return findShortestPath(start, end).getVertexList();
+    }
 
-    int getDistance(Station start, Station end);
+    protected void validateConnected(GraphPath<Station, DefaultWeightedEdge> path) {
+        if (path == null) {
+            throw new NotConnectedPathsException();
+        }
+    }
 
-    int getDuration(Station start, Station end);
+    public void validateConnected(Station start, Station end) {
+        if (findShortestPath(start, end) == null) {
+            throw new NotConnectedPathsException();
+        }
+    }
+
+    public void validateContains(Station start, Station end) {
+        if (!graph.containsVertex(start)) {
+            throw new NotAddedStartToPathsException(start.getName());
+        }
+        if (!graph.containsVertex(end)) {
+            throw new NotAddedEndToPathsException(end.getName());
+        }
+    }
+
+    protected GraphPath<Station, DefaultWeightedEdge> findShortestPath(Station start, Station end) {
+        DijkstraShortestPath<Station, DefaultWeightedEdge> dijkstraShortestPath = new DijkstraShortestPath<>(graph);
+        GraphPath<Station, DefaultWeightedEdge> path = dijkstraShortestPath.getPath(start, end);
+        validateConnected(path);
+        return path;
+    }
+
+    protected Section getSectionByEdge(DefaultWeightedEdge edge) {
+        Station upStation = graph.getEdgeSource(edge);
+        Station downStation = graph.getEdgeTarget(edge);
+
+        return sections.stream()
+                .filter(section -> upStation.equals(section.getUpStation()) && downStation.equals(section.getDownStation()))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("엣지에 해당하는 구간을 찾을 수 없습니다."));
+    }
+
+    protected abstract WeightedMultigraph<Station, DefaultWeightedEdge> createGraph(List<Section> sections);
+
+    public abstract int getDistance(Station start, Station end);
+
+    public abstract int getDuration(Station start, Station end);
 }
